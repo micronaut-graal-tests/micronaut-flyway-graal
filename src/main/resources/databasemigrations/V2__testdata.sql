974b8865e95706aac4877575210c419276d49eba
Iván López <lopezi@objectcomputing.com>
2020-04-27 17:15:07 +0200

    Use Micronaut Data JDBC and add Flyway migrations

---
+++ src/main/resources/databasemigrations/V2__testdata.sql
@@ -0,0 +1,2 @@
+INSERT INTO users(id, username, first_name, last_name) VALUES(1, 'ilopmar', 'Iván', 'López');
+INSERT INTO users(id, username, first_name, last_name) VALUES(2, 'graemerocher', 'Graeme', 'Rocher');
